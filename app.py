2baf44afdb1bb9dbce2b46d7a44ee26eabf5707e
theahihi <168960744+theahihi@users.noreply.github.com>
2024-05-28 00:13:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ st.sidebar.title('Nhập các thuộc tính khách hàng')
 
 
 customer_id = st.sidebar.number_input('Mã khách hàng', min_value=0, step=1)
-credit_score = st.sidebar.number_input('Điểm tín dụng', min_value=0, max_value=850, 300,step=1)
+credit_score = st.sidebar.number_input('Điểm tín dụng', min_value=0, max_value=900, 300,step=1)
 age = st.sidebar.number_input('Tuổi', min_value=18, max_value=100, step=1)
-balance = st.sidebar.number_input('Số dư tài khoản', min_value=0.0, max_value=100000000,5000000,step=1000.0, format="%.2f")
-estimated_salary = st.sidebar.number_input('Lương ước tính', min_value=0.0, max_value=100000000, 10000000 ,step=1000.0, format="%.2f")
+balance = st.sidebar.number_input('Số dư tài khoản', min_value=0.0,5000000,step=1000.0, format="%.2f")
+estimated_salary = st.sidebar.number_input('Lương ước tính', min_value=0.0, 10000000 ,step=1000.0, format="%.2f")
 
 # Make predictions
 input_data = np.array([[customer_id,age,credit_score,balance,estimated_salary]])

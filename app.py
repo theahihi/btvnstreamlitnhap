134bcb477d7585f880009adeaf0970122d6461f0
theahihi <168960744+theahihi@users.noreply.github.com>
2024-05-27 17:52:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained KNN model
-model_path = 'knn_model2.pkl'
+model_path = 'knn_model3.pkl'
 knn = pickle.load(open(model_path, 'rb'))
 
 # Title of the application

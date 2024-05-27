e96d13ec1b9140fdd099b8742116d4a4f2e4bd61
theahihi <168960744+theahihi@users.noreply.github.com>
2024-05-27 23:38:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.sidebar.title('Nhập các thuộc tính khách hàng')
 
 
 customer_id = st.sidebar.number_input('Mã khách hàng', min_value=0, step=1)
-credit_score = st.sidebar.number_input('Điểm tín dụng', min_value=300, max_value=850, step=1)
+credit_score = st.sidebar.number_input('Điểm tín dụng', min_value=0, max_value=850, step=1)
 age = st.sidebar.number_input('Tuổi', min_value=18, max_value=100, step=1)
 balance = st.sidebar.number_input('Số dư tài khoản', min_value=0.0, step=1000.0, format="%.2f")
 estimated_salary = st.sidebar.number_input('Lương ước tính', min_value=0.0, step=1000.0, format="%.2f")

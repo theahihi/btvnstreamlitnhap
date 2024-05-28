9c62ef808a062d07be322459fc3701f76532042c
theahihi <168960744+theahihi@users.noreply.github.com>
2024-05-28 22:55:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 # Load the trained KNN model
-model_path = 'bank_churn.pkl'
-knn = pickle.load(open(model_path, 'rb'))
+model_path = 'bankchurnhaha.pkl'
+clf = pickle.load(open(model_path, 'rb'))
 
 # Title of the application
 st.title('Dự đoán khách hàng rời bỏ ngân hàng')
@@ -17,15 +17,15 @@ st.sidebar.title('Nhập các thuộc tính khách hàng')
 
 
 
-customer_id = st.sidebar.number_input('Mã khách hàng', min_value=0, step=1)
+
 credit_score = st.sidebar.number_input('Điểm tín dụng', min_value=0, max_value=900, value=300, step=1)
 age = st.sidebar.number_input('Tuổi', min_value=18, max_value=100, step=1)
 balance = st.sidebar.number_input('Số dư tài khoản', min_value=0.0, value=500000.0, step=1000.0, format="%.2f")
 estimated_salary = st.sidebar.number_input('Lương ước tính', min_value=0.0, value=10000000.0, step=1000.0, format="%.2f")
 
 # Make predictions
-input_data = np.array([[customer_id,age,credit_score,balance,estimated_salary]])
-prediction = knn.predict(input_data)
+input_data = np.array([[age,credit_score,balance,estimated_salary]])
+prediction = clf.predict(input_data)
 
 # Display prediction
 st.write('## Kết quả dự đoán:')
